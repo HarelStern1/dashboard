adc2e60ca7caafd7abce674876b7b1842ca3d6a4
harel <harelstern1@gmail.com>
2023-05-14 22:33:40 +0300

    feat: autocomplete

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,27 +1,28 @@
-import { Select as MuiSelect } from "@mui/material";
+import { MenuItem, Select as MuiSelect } from "@mui/material";
 import { PropsWithChildren } from "react";
 import { Controller } from "react-hook-form";
 
 interface SelectProps {
   name: string;
   control: any;
-  defaultValue: string;
+  options: any[];
 }
 
-export const Select = ({
-  name,
-  control,
-  defaultValue,
-  children,
-}: PropsWithChildren<SelectProps>) => {
+export const Select = ({ name, control, options }: PropsWithChildren<SelectProps>) => {
+  const selectOptions = options.map(({ label, value }) => (
+    <MenuItem key={value} value={value}>
+      {label}
+    </MenuItem>
+  ));
+
   return (
     <Controller
       control={control}
       name={name}
-      defaultValue={defaultValue}
+      defaultValue={options[0].value}
       render={({ field: { value, onChange }, fieldState: { error } }) => (
         <MuiSelect value={value} onChange={onChange} error={!!error}>
-          {children}
+          {selectOptions}
         </MuiSelect>
       )}
     />
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,68 @@
+import { Box, Button, MenuItem } from "@mui/material";
+import { TextField, Select, Autocomplete } from "../../components";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    country: yup.string().required(),
+    movie: yup.string().required(),
+  })
+  .required();
+
+type FormData = yup.InferType<typeof schema>;
+
+const countries = [
+  {
+    label: "Israel",
+    value: "israel",
+  },
+  {
+    label: "United States",
+    value: "usa",
+  },
+];
+
+const movies = [
+  {
+    label: "Pulp Fiction",
+    id: "best movie ever",
+  },
+  {
+    label: "Fight Club",
+    id: "Dont talk about it",
+  },
+];
+
+const formFields = [
+  { name: "firstName", component: TextField },
+  { name: "lastName", component: TextField },
+  { name: "country", options: countries, component: Select },
+  { name: "movie", options: countries, component: Autocomplete },
+];
+
 const Home = () => {
-  return <div></div>;
+  const { control, handleSubmit } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => console.log(data);
+
+  const fields = formFields.map((field) => {
+    const { name, options, component: Component } = field;
+
+    return <Component key={name} name={name} control={control} options={options || []} />;
+  });
+
+  return (
+    <Box component="form" sx={{ padding: "50px" }} onSubmit={handleSubmit(onSubmit)}>
+      {fields}
+      <Button type="submit">Submit</Button>
+    </Box>
+  );
 };
 
 export default Home;
